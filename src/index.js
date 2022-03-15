5f23047ad3f5b2c3ce5ac8aeb55da02f5b158472
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-03-15 23:49:19 +0530

    15March validation assignment

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const userController = require("./controllers/user.controllers.js");
+app.use("/users", userController);
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+
+
